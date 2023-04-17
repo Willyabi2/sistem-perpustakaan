8d1bad708c4caaf93403eb3b8e77d135e62641f8
Willyabi2 <129682501+Willyabi2@users.noreply.github.com>
2023-04-17 17:13:38 +0700

    Add files via upload

---
+++ resources/views/banner.blade.php
@@ -0,0 +1,5 @@
+<!-- BANNER awal-->
+<div class="col-md-12">
+    <img src="{{ asset('gambar') }}/banner.jpg" width="100%" height="280px">
+</div>
+<!-- BANNER akhir-->
---
+++ resources/views/konten.blade.php
@@ -0,0 +1,7 @@
+<!------------KONTEN------------->
+<div class="col-md-9">
+    @yield('isihalaman')
+</div>
+<!---------------->
+</div>
+</div>
